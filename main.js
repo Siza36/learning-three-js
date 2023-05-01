4b02338feca9b431d280ed6fa7689a49e1426da5
Siza <ali.zarshenas.2018@gmail.com>
2023-05-01 19:38:47 +0430

    Drawing Lines in three.js

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGL1Renderer();
 
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0,0,100);
+camera.lookAt(0,0,0);
+
+const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth , window.innerHeight);
 
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 0xffffff});
+const material = new THREE.LineBasicMaterial( {color:0xffffff} )
 
-const cube = new THREE.Mesh(geometry,material );
+const points = [];
+points.push(new THREE.Vector3(-10,0,0));
+points.push(new THREE.Vector3(0,10,0));
+points.push(new THREE.Vector3(10,0,0));
 
-scene.add(cube);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-camera.position.z =2;
+const line = new THREE.Line(geometry,material);
 
-function animate(){
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01; 
-    cube.rotation.y += 0.01; 
-
-    renderer.render(scene , camera);
-}
-animate()
+scene.add(line);
+renderer.render(scene,camera)
